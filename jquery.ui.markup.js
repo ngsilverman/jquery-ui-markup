585eedfc4aa0fda894dd1732301e358acf8976c6
Nathanael Silverman <nathanael.silverman@gmail.com>
2013-02-16 10:18:49 +0100

    Transform hyphenated attributes into valid option names.

--- jquery.ui.markup.js
+++ jquery.ui.markup.js
@@ -3,13 +3,20 @@
 var namespace = "data-ui",
 	optionsNamespace = namespace+"-";
 
+function attrToOption( attr ) {
+	attr = attr.substring( optionsNamespace.length ).toLowerCase();
+	return attr.replace(/-.?/g, function( string ) {
+		return string.charAt( 1 ).toUpperCase();
+	});
+}
+
 $( document ).ready(function() {
 	$( "["+namespace+"]" ).each(function() {
 		var attr, i, options = {};
 		for ( i = 0; i < this.attributes.length; i++ ) {
 			attr = this.attributes[i];
 			if ( attr.name.indexOf( optionsNamespace ) === 0 && attr.name.length > optionsNamespace.length ) {
-				options[ attr.name.substring( optionsNamespace.length ) ] = attr.value;
+				options[ attrToOption( attr.name ) ] = attr.value;
 			}
 		}
 
