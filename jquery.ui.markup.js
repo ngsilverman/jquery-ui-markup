99c13006eaf18c93a958c91ac3a010ec796eaadd
Nathanael Silverman <nathanael.silverman@gmail.com>
2013-02-16 10:38:14 +0100

    Conform to coding guidelines.

--- jquery.ui.markup.js
+++ jquery.ui.markup.js
@@ -5,14 +5,15 @@ var namespace = "data-ui",
 
 function attrToOption( attr ) {
 	attr = attr.substring( optionsNamespace.length ).toLowerCase();
-	return attr.replace(/-.?/g, function( string ) {
+	return attr.replace( /-.?/g, function( string ) {
 		return string.charAt( 1 ).toUpperCase();
 	});
 }
 
 $( document ).ready(function() {
 	$( "["+namespace+"]" ).each(function() {
-		var attr, i, options = {};
+		var attr, i, options = {}, $this;
+
 		for ( i = 0; i < this.attributes.length; i++ ) {
 			attr = this.attributes[i];
 			if ( attr.name.indexOf( optionsNamespace ) === 0 && attr.name.length > optionsNamespace.length ) {
@@ -20,7 +21,7 @@ $( document ).ready(function() {
 			}
 		}
 
-		var $this = $( this );
+		$this = $( this );
 		$this[ $this.data("ui") ]( options );
 	});
 });
